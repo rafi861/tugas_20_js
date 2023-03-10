d168fc8e3ce3fe7b1599569a00414b44b4a98619
Muhammad Rafi <muhammadrafi2545@gmail.com>
2023-03-10 18:14:02 +0700

    tugas 20 js

---
+++ tugas20.js
@@ -0,0 +1,9 @@
+function tugas() {
+  var lorem = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+  console.log(lorem.match("are"));
+  var ipsum = null;
+  console.log(ipsum);
+  var dummy = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+  console.log(dummy.match("words"));
+}
+tugas()
